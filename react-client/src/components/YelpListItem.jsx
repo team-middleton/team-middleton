91dee394ec5947455434ed2a0680f9ad16079b99
Hack Reactor Student <student@hackreactor.com>
2018-02-20 16:19:44 -0500

    saved changes, added fields to our help data

--- react-client/src/components/YelpListItem.jsx
+++ react-client/src/components/YelpListItem.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function YelpListItem (props) {
-    console.log('are we runnign yelpw list item')
     return (
         <div>
-            <h2> check it fresh </h2>
+            <h4> {props.business.name} </h4>
+            <h6> {props.business.address.address+", "} 
+                {props.business.address.city+", "} 
+                {props.business.address.state} </h6>
+            <h6> {props.business.phone}</h6>
+            <h6> Rating: <span></span> {props.business.rating}<span></span> stars </h6>
+
         </div>
 
     )
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import List from './components/List.jsx';
+
 import YelpList from './components/YelpList.jsx';
 
 class App extends React.Component {
@@ -19,7 +19,7 @@ class App extends React.Component {
   render () {
     return (<div>
       <h1>Item List</h1>
-      <List items={this.state.items}/>
+
       <YelpList />
     </div>)
   }
