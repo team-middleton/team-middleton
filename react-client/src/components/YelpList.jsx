8497e7e9f779b88cbc3cc7c364f0ae28bd7a748a
Hack Reactor Student <student@hackreactor.com>
2018-02-22 13:13:37 -0500

    committed to rebase and see changes again

--- react-client/src/components/YelpList.jsx
+++ react-client/src/components/YelpList.jsx
@@ -57,8 +57,8 @@ class YelpList extends React.Component {
   }
 
   componentDidMount() {
-    this.getYelpServices()
-    // this.getZipCodeServices();
+    // this.getYelpServices()
+    this.getZipCodeServices();
   }
 
   handleChange(e) {
