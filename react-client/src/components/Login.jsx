a1e746b7957e67bc0b886c52014fd920cadb200f
Marcus Hausler <marcus.hausler@gmail.com>
2018-02-21 19:37:46 -0500

    Added post requests to Login and Signup pages

--- react-client/src/components/Login.jsx
+++ react-client/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class Login extends React.Component {
 	constructor(props){
@@ -7,29 +8,28 @@ class Login extends React.Component {
 			username: '',
 			password: ''
 		}
-		this.onChange = this.onChange.bind(this);
 		this.login = this.login.bind(this);
 	}
 
-	onChange(e){
-		e.preventDefault();
-		//set state to the user's input
-		this.setState({
-			e.target.id: e.target.value
-		})
-	}
-
 	login(e) {
-		e.preventDefault();
-		//send the state on
+		axios.post('/login', {
+			username: this.state.username,
+			password: this.state.password
+		})
+		.then((response) => {
+			//route them somewhere and change App's state to logged in
+		})
+		.catch((err) => {
+			console.error(err)
+		})
 	}
 
 	render(){
 		return(<div>
 			<form>
-			Username: <input value={this.state.username} onChange(e) => {this.onChange(e)}/>
-			Password: <input value={this.state.password} onChange(e) => {this.onChange(e)}/>
-			<button onClick={this.login}>Login</button>
+			Username: <input value={this.state.username} onChange={(event) => this.setState({username: event.target.value})}/>
+			Password: <input value={this.state.password} onChange={(event) => this.setState({password: event.target.value})}/>
+			<button type="submit" onClick={this.login}>Login</button>
 			</form>
 		</div>)
 	}
