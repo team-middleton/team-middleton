c68516f2ffb38de13ea74f9118517b596bdc4491
Marcus Hausler <marcus.hausler@gmail.com>
2018-02-22 12:27:45 -0500

    Hooked Login and Signup components up to App

--- react-client/src/components/Signup.jsx
+++ react-client/src/components/Signup.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Axios from 'axios';
+import axios from 'axios';
 
 class Signup extends React.Component {
 	constructor(props){
@@ -20,7 +20,7 @@ class Signup extends React.Component {
 			zipcodefrom: this.state.zipcode
 		})
 		.then((response) => {
-			//route them somewhere
+			console.log('You\'re signed up! Now just log in.')
 		})
 		.catch((error) => {
 			console.error(error)
@@ -34,7 +34,7 @@ class Signup extends React.Component {
 				Username: <input value={this.state.username} onChange={(event) => this.setState({username: event.target.value})}/>
 				Password: <input value={this.state.password} onChange={(event) => this.setState({password: event.target.value})}/>
 				Zipcode: <input value={this.state.zipcode} onChange={(event) => this.setState({zipcode: event.target.value})}/>
-				<button type="submit" onClick={this.signup}>Sign Up!</button>
+				<button type="submit" onClick={this.signup()}>Sign Up!</button>
 				</form>
 			</div>
 		)
