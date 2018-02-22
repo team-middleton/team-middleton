2c91840c6da79ade669aef0a5fb54d04f8dd2603
Hack Reactor Student <student@hackreactor.com>
2018-02-22 13:38:10 -0500

    changed some white space

--- react-client/src/components/GoogleMap.jsx
+++ react-client/src/components/GoogleMap.jsx
@@ -4,20 +4,20 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker , defaultCenter} from "r
 
 const GoogleMaps = withScriptjs(withGoogleMap((props) => (
 
-<GoogleMap
-    defaultZoom={14}
-    defaultCenter={{ lat: 40.755603, lng: -73.984931 }}
-    center={{ lat: props.businesses[0].coordinates.latitude, lng: props.businesses[0].coordinates.longitude }}
+  <GoogleMap
+      defaultZoom={14}
+      defaultCenter={{ lat: 40.755603, lng: -73.984931 }}
+      center={{ lat: props.businesses[0].coordinates.latitude, lng: props.businesses[0].coordinates.longitude }}
   >
 
-    {props.isMarkerShown && 
-      props.businesses.map((business, i) => {
-        return <Marker 
-          key={props.businesses[i].name}
-          position={{ lat: props.businesses[i].coordinates.latitude, lng: props.businesses[i].coordinates.longitude }} 
-        />
-      })
-    }
+      {props.isMarkerShown && 
+        props.businesses.map((business, i) => {
+          return <Marker 
+            key={props.businesses[i].name}
+            position={{ lat: props.businesses[i].coordinates.latitude, lng: props.businesses[i].coordinates.longitude }} 
+          />
+        })
+      }
 
   </GoogleMap>
 
