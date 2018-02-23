08709f03509f3c91a4738b71b2d89ff86ba601f7
Marcus Hausler <marcus.hausler@gmail.com>
2018-02-23 14:38:06 -0500

    Added option for environment variable as database url

--- database-mysql/index.js
+++ database-mysql/index.js
@@ -3,14 +3,14 @@ var bcrypt = require('bcrypt');
 
 
 var connection = mysql.createConnection({
-  host     : 'moving.cgipzmxzylvi.us-east-1.rds.amazonaws.com',
+  host     : Process.env.DB_URL || 'moving.cgipzmxzylvi.us-east-1.rds.amazonaws.com',
   user     : 'middleton',
   password : 'h4ckr34t0r',
   database : 'moving'
 });
 
 connection.connect((err) => {
-  if (err) { console.log('error connecting to db', err); return; }
+  if (err) { console.log('error connecting to db'); return; }
   console.log('connected to the db');
 });
 
