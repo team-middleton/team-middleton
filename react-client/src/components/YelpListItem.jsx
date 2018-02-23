0cfe3e3e73324cc01c7ed06acf9a85abf770d61b
Hack Reactor Student <student@hackreactor.com>
2018-02-23 12:35:34 -0500

    need to pass index of item hovered into map marker

--- react-client/src/components/YelpListItem.jsx
+++ react-client/src/components/YelpListItem.jsx
@@ -4,7 +4,15 @@ import ReactDOM from 'react-dom';
 function YelpListItem (props) {
     return (
         <div className="yelpItems">
-            <h5> <a href={props.business.url} target='_blank'> {(props.index + 1).toString()+'.'} <span></span> {props.business.name}</a> </h5>
+            <h5> <a href={props.business.url} target='_blank' 
+            onMouseEnter={props.onMouseEnter.bind(this, props.index)}
+            onMouseLeave={props.onMouseLeave.bind(this, props.index)}   
+               > 
+               {props.letter+'.'} 
+            <span></span> 
+            {props.business.name}
+            </a> </h5>
+
             <h6> {props.business.address[0]}<span></span> {props.business.address.slice(1)}</h6>
             <h6> {props.business.phone}</h6>
             <h6> Rating: <span></span> {props.business.rating}<span></span> stars </h6>
