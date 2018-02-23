dacc01ff47afaad6e450fc426ba31e3b09f0ed34
Marcus Hausler <marcus.hausler@gmail.com>
2018-02-23 14:15:51 -0500

    Got Yelp API working again

--- server/yelpAPI.js
+++ server/yelpAPI.js
@@ -1,1 +1,3 @@
-module.exports = 'x7T5oUXhDkDfOk2mt1wfFEwTswpx3XjIRpx_KzWdAiw-x4APiOyPlSzgDIZG-Epm7COaSs8xFxyEL56ISBKif8QjhOeepMK-8ZiRJUtoCFHWKxgGp02NXaOxAY2MWnYx'
+module.exports = {
+  yelpAPI: 'x7T5oUXhDkDfOk2mt1wfFEwTswpx3XjIRpx_KzWdAiw-x4APiOyPlSzgDIZG-Epm7COaSs8xFxyEL56ISBKif8QjhOeepMK-8ZiRJUtoCFHWKxgGp02NXaOxAY2MWnYx'
+}
