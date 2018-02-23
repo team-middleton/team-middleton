f92c1f69a4754f4e4a534afed9193d13c1fe41b5
Allegra Berndt <allegra.berndt@gmail.com>
2018-02-23 08:54:31 -0500

    Fixed up parallax

--- react-client/src/components/Parallax.jsx
+++ react-client/src/components/Parallax.jsx
@@ -3,11 +3,11 @@ import van from '../../dist/van.png';
 import road from '../../dist/road.png'
 import React from 'react'
 const ParallaxImage = () => (
-    <div>
+    <div className="parallax">
     <Parallax
         className="truck"
-        offsetYMax={70}
-        offsetYMin={-70}
+        offsetYMax={30}
+        offsetYMin={-30}
         offsetXMax={-30}
         offsetXMin={30}
         slowerScrollRate
@@ -17,10 +17,8 @@ const ParallaxImage = () => (
     </Parallax>
     <Parallax
         className="road"
-        offsetYMax={70}
-        offsetYMin={-70}
-        offsetXMax={-30}
-        offsetXMin={30}
+        offsetYMax={6}
+        offsetYMin={-6}
         slowerScrollRate
         tag="figure"
     >
