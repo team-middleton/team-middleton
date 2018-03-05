db27ec5fd0a1caa60d65766c4379bb1705b2cabe
Juan Galan <juangalan@Juans-MacBook-Pro.local>
2018-03-05 14:46:48 -0500

    saved work many comments added

--- react-client/src/components/GoogleMap.jsx
+++ react-client/src/components/GoogleMap.jsx
@@ -4,16 +4,28 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker , defaultCenter} from "r
 
 const GoogleMaps = withScriptjs(withGoogleMap((props) => (
 
+  // these are just default coordinates as a reference; I think they are to the empire state building
+  // the actual center is set to the location of the first item in our services list 
+
+  // this is a conditional that renders a bouncing marker or a static one
+  // based on if that item is hovered over
+
+  //FYI - under react-client->dist->bluemapmarkers, 
+  //I have saved a blue marker for each the letters of the alphabet
+  // the aim was to make the bouncing marker blue when the user hovers over it
+  // to do so: it involves setting the value of icon to that image
+  // but this was a bit more complicated than I expected so I abandoned ship
   <GoogleMap
       defaultZoom={14}
       defaultCenter={{ lat: 40.755603, lng: -73.984931 }}
       center={{ lat: props.businesses[0].coordinates.latitude, lng: props.businesses[0].coordinates.longitude }}
   >
-
       {props.isMarkerShown && 
         props.businesses.map((business, i) => {
             var letter =  String.fromCharCode(65 + i);
             if (props.hovered === i) {
+
+
               return ( <Marker 
               animation = {google.maps.Animation.BOUNCE}
               key={props.businesses[i].name}
--- react-client/src/components/YelpListItem.jsx
+++ react-client/src/components/YelpListItem.jsx
@@ -5,15 +5,19 @@ function YelpListItem (props) {
     return (
         <div className="yelpItems">
             <h5> <a href={props.business.url} target='_blank' 
+            // target_blank makes it so that when a link is clicked, it's opened in a new tab
             onMouseEnter={props.onMouseEnter.bind(this, props.index)}
             onMouseLeave={props.onMouseLeave.bind(this, props.index)}   
                > 
-               {props.letter+'.'} 
-            <span></span> 
-            {props.business.name}
+               {props.letter+'. '} 
+                {props.business.name}
             </a> </h5>
 
             <h6> {props.business.address[0]}<span></span> {props.business.address.slice(1)}</h6>
+            {/* to note: the phone number is formatted in the server side in the utils 
+            file. could be a good thing to fix, as I essentially added parentheses and dashes in the right
+            places, which doesn't account for if we receive a number in a different format
+            */}
             <h6> {props.business.phone}</h6>
             <h6> Rating: <span></span> {props.business.rating}<span></span> stars </h6>
 
